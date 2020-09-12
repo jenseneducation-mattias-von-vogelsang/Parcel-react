63bf153b71a4f701507002772d7efcac0b9c33c5
Artur <Artur.Hoppner@hotmail.com>
2020-09-12 22:24:07 +0200

    added --no-cache, new test component

---
+++ src/components/ArturTest/ArturTest.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ArturTest extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>testing with React.Fragment</h1>;
+      </React.Fragment>
+    );
+  }
+}
