07bd64292bfa4e979c982eb20154711fbc43100a
Artur <Artur.Hoppner@hotmail.com>
2020-09-13 08:03:02 +0200

    added props to arturTest

--- src/components/ArturTest/ArturTest.js
+++ src/components/ArturTest/ArturTest.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
 
 export default class ArturTest extends React.Component {
   render() {
+    /* Used external variable value and passed it into React.Fragment
+		instead of wrapping it with a div.
+		
+		The second h1 is a props that gets its data from App.js*/
+    const reactFragmentTest = 'Testing with React.Fragment';
     return (
       <React.Fragment>
-        <h1>testing with React.Fragment</h1>;
+        <h1>{reactFragmentTest}</h1>;<h1>{this.props.greeting}</h1>
       </React.Fragment>
     );
   }
